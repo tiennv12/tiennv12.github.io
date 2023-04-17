d2cfdaa7680562971837acd41eabfb787093f456
Hoc Lap Trinh <vtmbmt11231111@gmail.com>
2023-04-17 16:53:11 +0700

    search commit

--- src/component/pages/Navbar.jsx
+++ src/component/pages/Navbar.jsx
@@ -1,92 +1,102 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import './navbar.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './navbar.css';
+import { SearchBar } from './Search/SearchBar';
+import { SearchResultsList } from './Search/SearchResutsList';
 
 export default function Navbar() {
- 
-  return (
-    <div>
-      <nav className="navbar navbar-expand-lg bg-white py-3 shadow-sm ">
-        <div className="container">
-          <a className="navbar-brand fw-bold fs-3" href="#">
-            HONDA ÔTÔ ĐẮK LẮK
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse fs-3 " id="navbarSupportedContent">
-            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Trang chủ
-                </a>
-              </li>
-              
-              <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle"
-                  data-bs-toggle="dropdown"
-                  to="./following"
-                  role="button"
-                  aria-expanded="false"
-                >
-                  Sản Phẩm
-                </Link>
-                <ul className="dropdown-menu">
-                <li>
-                    <Link className="dropdown-item"  to="./following/civicr">
-                      Honda Civic Type R
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item"  to="./following/accord">
-                      Honda Accrod
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="./following/civic">
-                      Honda Civic
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="./following/crv">
-                      Honda Crv
-                    </Link>
-                  </li>
-                  
-                  <li>
-                    <Link className="dropdown-item" to="./following/hrv">
-                      Honda Hrv
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="./following/city">
-                      Honda City
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Tin Tức
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Liên Hệ</a>
-              </li>
-            </ul>
-            
-          </div>
+    const [results, setResults] = useState([]);
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-white py-3 shadow-sm ">
+                <div className="container">
+                    <a className="navbar-brand fw-bold fs-3" href="#">
+                        HONDA ÔTÔ ĐẮK LẮK
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse fs- " id="navbarSupportedContent">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">
+                                    Trang chủ
+                                </a>
+                            </li>
+
+                            <li className="nav-item dropdown">
+                                <Link
+                                    className="nav-link dropdown-toggle"
+                                    data-bs-toggle="dropdown"
+                                    to="./following"
+                                    role="button"
+                                    aria-expanded="false"
+                                >
+                                    Sản Phẩm
+                                </Link>
+                                <ul className="dropdown-menu">
+                                    <li>
+                                        <Link className="dropdown-item" to="./following/civicr">
+                                            Honda Civic Type R
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link className="dropdown-item" to="./following/accord">
+                                            Honda Accrod
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link className="dropdown-item" to="./following/civic">
+                                            Honda Civic
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link className="dropdown-item" to="./following/crv">
+                                            Honda Crv
+                                        </Link>
+                                    </li>
+
+                                    <li>
+                                        <Link className="dropdown-item" to="./following/hrv">
+                                            Honda Hrv
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link className="dropdown-item" to="./following/city">
+                                            Honda City
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Tin Tức
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Liên Hệ
+                                </a>
+                            </li>
+                        </ul>
+
+                        <div className="search-bar-container">
+                            <SearchBar setResults={setResults} />
+                            {results && results.length > 0 && <SearchResultsList results={results} />}
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
-      </nav>
-    </div>
-  );
+    );
 }
---
+++ src/component/pages/Search/SearchBar.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+import "./SearchBar.css";
+
+export const SearchBar = ({ setResults }) => {
+  const [input, setInput] = useState("");
+
+  const fetchData = (value) => {
+    fetch("http://localhost:9200/products/_search")
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.filter((products) => {
+          return (
+            value &&
+            products &&
+            products.title &&
+            products.title.toLowerCase().includes(value)
+          );
+        });
+        setResults(results);
+      });
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+
+  return (
+    <div className="input-wrapper">
+      <FaSearch id="search-icon" />
+      <input
+        placeholder="Type to search..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </div>
+  );
+};
