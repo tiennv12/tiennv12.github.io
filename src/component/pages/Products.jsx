1d82975f21d55f49e762c8f31a838f1eaa917e85
Hoc Lap Trinh <vtmbmt11231111@gmail.com>
2023-04-29 09:11:36 +0700

    searchbar commit

--- src/component/pages/Products.jsx
+++ src/component/pages/Products.jsx
@@ -10,13 +10,13 @@ export default function Products() {
     useEffect(() => {
         const getProducts = async() =>{
             setLoading(true);
-            const response =await fetch("https://test-api.lthoang.com/products");
-            if(componentMounted){
-                setData(await response.clone().json());
-                setFilter(await response.json());
-                setLoading(false);
-                console.log(filter)
-            }
+            // const response =await fetch("https://test-api.lthoang.com/products");
+            // if(componentMounted){
+            //     setData(await response.clone().json());
+            //     setFilter(await response.json());
+            //     setLoading(false);
+            //     console.log(filter)
+            // }
             return () =>{
                 componentMounted =false;
             }
--- src/component/pages/Search/SearchBar.jsx
+++ src/component/pages/Search/SearchBar.jsx
@@ -1,30 +1,74 @@
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+// import elasticsearch from "elasticsearch";
+
 
 import "./SearchBar.css";
 
 export const SearchBar = ({ setResults }) => {
   const [input, setInput] = useState("");
+  // const url ='http://localhost:9200/products/_search';
+  // const username = 'elastic';
+  // const password = 'Password';
 
+  
   const fetchData = (value) => {
-    fetch("http://localhost:9200/products/_search")
-      .then((response) => response.json())
-      .then((json) => {
-        const results = json.filter((products) => {
-          return (
-            value &&
-            products &&
-            products.title &&
-            products.title.toLowerCase().includes(value)
-          );
-        });
-        setResults(results);
-      });
-  };
+    var requestOptions = {
+      method: 'GET',
+      mode: 'no-cors'
+    };
+    
+    return fetch("http://localhost:9200/products/_search/", requestOptions)
+      .then(response => {
+        console.log(response);
+        const temp = response.text();
+        console.log(temp);
+        return temp;
+      })
+      // .then(result => console.log(result))
+      // .catch(error => console.log('error', error));
+    // fetch('http://localhost:9200/products/_search', {
+    //   method:'GET', 
+    //   mode: 'no-cors',
+    //   headers: new Headers({
+    //     'Content-Type': 'application/json'
+    //   })
+    // }).then(res => {console.log(res.json())})
+    // .then(response => {
+    //   console.log(response);
+    // });
+   
+      
+      // .then((json) => {
+      //   const results = json.filter((products) => {
+      //     return (
+      //       value &&
+      //       products &&
+      //       products.title &&
+      //       products.title.toLowerCase().includes(value)
+      //     );
+      //   });
+      //   setResults(results);
+      // });
+ };
 
   const handleChange = (value) => {
     setInput(value);
     fetchData(value);
+    // const client = new elasticsearch.Client({
+    //   host: 'http://localhost:9200',
+    //   });
+    //    client.search({
+    //     index: 'products/_search',
+    //     Authorization :{
+    //       username : 'elastic',
+    //       password :'Password'
+    //     } ,
+    //     query: {
+    //       match: { quote: value }
+    //     }
+    //   }).then(response => response.json())
+    //    
   };
 
   return (
