8fdebbd154cd78f3546ba2fd5848f4473bfc6ad7
Hoc Lap Trinh <vtmbmt11231111@gmail.com>
2023-05-31 16:32:36 +0700

    fix product commit

--- src/component/pages/Followings/Products.jsx
+++ src/component/pages/Followings/Products.jsx
@@ -10,7 +10,7 @@ export default function Products() {
     useEffect(() => {
         const getProducts = async() =>{
             setLoading(true);
-            const response =await fetch("https://test-api.lthoang.com/products");
+            const response =await fetch("https://test-api.lthoang.com/hondas");
             if(componentMounted){
                 setData(await response.clone().json());
                 setFilter(await response.json());
